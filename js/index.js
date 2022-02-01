ed42f34035d8f53154383908ea17a379522c3176
hilmanadi <hilmanadi1995@gmail.com>
2022-02-01 23:49:19 +0700

    v2

--- js/index.js
+++ js/index.js
@@ -1,63 +1,14 @@
-var check = () =>{
-  $('.sidenav').sidenav('open');
+var getmenu = (x) => {
+	console.log(x);
+	if(x=="Introduction"){
+		window.location.href='#introduction';
+	}else if(x=='Profile'){
+		window.location.href='#profile';
+	}else if(x=='Experience'){
+		window.location.href='#experience';
+	}else if(x=='Achievement'){
+		window.location.href='#achievement';
+	}else if(x=='Projects'){
+		window.location.href='#projects';
+	}
 }
-
-var back = () =>{
-	$('.sidenav').sidenav('close');
-}
-
-var prof = () => {
-	window.location.href = '#profile';
-}
-
-var exp = () => {
-	window.location.href = '#experience';
-}
-
-var ach = () => {
-	window.location.href = '#achievement';
-}
-
-var pro = () => {
-	window.location.href = '#projects';
-}
-
-var intro = () => {
-	window.location.href = '#welcome';
-}
-
-$(document).ready(function(){
-$('.sidenav').sidenav({
-	draggable:true
-});
-
-$('#burgerbutton').click(function(){
-	check();
-});
-
-$('#so-back').click(function(){
-	back();
-});
-
-$('#so-prof').click(function(){
-	prof();
-});
-
-$('#so-exp').click(function(){
-	exp();
-});
-
-$('#so-ach').click(function(){
-	ach();
-});
-
-$('#so-pro').click(function(){
-	pro();
-});
-
-$('#so-intro').click(function(){
-	intro();
-});
-
-
-});
