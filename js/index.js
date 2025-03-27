11318cfc3a86b43b3f8248a696df9d6e9cd39865
Hilman Adi K <hilmanadi1995@gmail.com>
2025-03-27 11:18:54 +0700

    2025

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+
+var getYear = () =>{
+	const date1 = new Date();
+	const date2 = new Date('10/11/1995');
+	const diffTime = Math.abs(date2 - date1);
+	const diffYear = Math.floor(diffTime / (1000 * 60 * 60 * 24)/11); 
+	
+	return diffYear
+}
+
 var getmenu = (x) => {
 	if(x=="Introduction"){
 		window.location.href='#introduction';
@@ -9,5 +19,7 @@ var getmenu = (x) => {
 		window.location.href='#achievement';
 	}else if(x=='Projects'){
 		window.location.href='#projects';
+	}else if(x=='Skills'){
+		window.location.href='#skills';
 	}
 }
